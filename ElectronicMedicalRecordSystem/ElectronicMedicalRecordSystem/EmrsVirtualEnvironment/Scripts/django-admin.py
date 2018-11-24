2064769f16c583d552f653fcf8be3a9a4133ed17
rdsalutan <rdsalutan@gmail.com>
2018-11-24 12:12:28 +0800

    Initial Commit

---
+++ ElectronicMedicalRecordSystem/ElectronicMedicalRecordSystem/EmrsVirtualEnvironment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\rdsalutan\Source\Repos\ElectronicMedicalRecordSystem\ElectronicMedicalRecordSystem\ElectronicMedicalRecordSystem\EmrsVirtualEnvironment\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
